f23fde41ff1cce113d14488f947420e95986f450
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2023-09-12 14:08:01 -0500

    Multiple plate detectien available with reagion restrictions, adding code

--- PersonalProyect/PlateDetection.py
+++ PersonalProyect/PlateDetection.py
@@ -4,12 +4,28 @@ import numpy as np
 lower_yellow = (20, 120, 70)
 upper_yellow = (30, 255, 255)
 
-capture = cv.VideoCapture("Parqueadero.mp4")
+capture = cv.VideoCapture(0)
     
     
 def detectPlate(frame, blank_img, second_blank_img):
+    
+    # Gitting cropped image fragment
+    h, x, c = frame.shape
+    
+    # New image analysis
+    # Y
+    x1 = int(h /3)
+    x2 = (x1 * 2)
+    
+    # X configurations
+    y1_len = (x/2)
+    y1 = int(x/3 - y1_len/6)
+    y2 = int(y1 + y1_len)
+    
+    cropped_img = frame[x1:x2, y1:y2]
+    cv.imshow("Cropped", cropped_img)
     # Convert into HSV format
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    hsv = cv.cvtColor(cropped_img, cv.COLOR_BGR2HSV)
     
     # threshold video
     threshold = cv.inRange(hsv, lower_yellow, upper_yellow)
@@ -17,28 +33,25 @@ def detectPlate(frame, blank_img, second_blank_img):
     
     # getting image contours
     contours, hierarchy = cv.findContours(threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    
-    # plates_contours = []
-    
-    # for c in contours:
-    #     if cv.contourArea(c) > 2000:
-    #         plates_contours.append(c)
         
     # Drawing identified contours
+
     cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
     cv.imshow("Contours image", blank)
     
     for i in contours:
         plate_area = cv.contourArea(i)
-        if plate_area > 2000:
+        print(f"{plate_area}")
+        if plate_area > 35:
             x, y, w, h = cv.boundingRect(i)
-            cropped_plate = frame[y:y+h, x:x+w]
-            cv.rectangle(frame, (x,y), (x + w, y + h), (36, 255, 12), 2)
+            # cropped_plate = frame[y:y+h, x:x+w]
+            cv.rectangle(frame, (y1 + x, x1 + y), (y1 +x + w, x1 + y + h), (36, 255, 12), 2)
+            # cv.rectangle(frame, (x,y), (x + w, y + h), (36, 255, 12), 2)
             
             
     print(f"{contours}")
     cv.imshow("Video", frame)
-    cv.imshow("Cropped", cropped_plate)
+    # cv.imshow("Cropped", cropped_plate)
     
     
 
